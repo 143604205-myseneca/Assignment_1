2774b05d9336fed11dbd38dd36d8c3ad9a6a663c
143604205-myseneca <skandhare@myseneca.ca>
2024-02-04 23:51:07 +0000

    EC2 instance and ECR created

---
+++ terraform_code/main.tf
@@ -0,0 +1,117 @@
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_instance" "my_amazon" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = (
+    {
+      "Name" = "clo835-Amazon-Linux"
+    }
+  )
+}
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "my_key" {
+  key_name   = "assignment1"
+  public_key = file("assignment1.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "allow ssh and http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom HTTP"
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom HTTP"
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom HTTP"
+    from_port        = 8083
+    to_port          = 8083
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = (
+    {
+      "Name" = "clo835-Amazon-Linux"
+    }
+  )
+}
+
+#Create ECR
+resource "aws_ecr_repository" "my_ecr_repo" {
+
+  name = "soham-assignment1"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}  
